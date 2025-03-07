f84a65b68582b742205711c8fc92303c25640015
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-03-06 16:07:51 -0800

    Created unique app key

--- ignition/__init__.py
+++ ignition/__init__.py
@@ -1,6 +1,15 @@
 """Global variables for the various scripts."""
 
-CLIENT_PRIVATE_KEY = "poc_test_ignition_client_key.pem"
-CLIENT_CERTIFICATE = "poc_test_ignition_client_cert.der"
+# Import packages
+import getpass
+import socket
+
+
+HOSTNAME = "".join(_.upper() for _ in socket.gethostname() if _.isalnum())
+USERNAME = "".join(_.upper() for _ in getpass.getuser() if _.isalnum())
+
 SERVER_CERTIFICATE = "ignition-server.der"
-CLIENT_APP_IDENTIFIER = "Tag:Read:Test"
+CLIENT_PREFIX = "Ignition-OPCUA-Client-POC-Test"
+CLIENT_APP_IDENTIFIER = f"{CLIENT_PREFIX}-{HOSTNAME}-{USERNAME}"
+CLIENT_PRIVATE_KEY = f"{CLIENT_APP_IDENTIFIER}-key.pem"
+CLIENT_CERTIFICATE = f"{CLIENT_APP_IDENTIFIER}-cert.der"
